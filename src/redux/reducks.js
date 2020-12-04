6d204a0925f57c82561686faaa467c3d6ea187a2
innfi <innfi.world@gmail.com>
2020-12-04 23:44:31 +0900

    react-redux: base settings

---
+++ src/redux/reducks.js
@@ -0,0 +1,51 @@
+import { createStore, combineReducers } from 'redux';
+
+
+const SIGN_IN = 'SIGN_IN';
+const SIGN_UP = 'SIGN_UP';
+
+//state models
+
+const initialState = {
+    userId: '',
+    nickname: '',
+    email: ''
+};
+
+//reducers
+
+const accountReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case SIGN_IN:
+            return { //FIXME
+
+            };
+        case SIGN_UP:
+            return { //FIXME
+
+            };
+        default: return state;
+    }
+};
+
+const reducers = combineReducers(accountReducer);
+
+//actions 
+
+export const signUp = (signUpInfo) => ({
+    type: SIGN_UP, 
+    payload: {
+        signUpInfo
+    }
+});
+
+export const signIn = (signInInfo) => ({
+    type: SIGN_IN,
+    payload: {
+        signInInfo
+    }
+});
+
+//store
+
+export const store = createStore(reducers);
