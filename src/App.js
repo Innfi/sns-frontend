d425c87ec3e4a157bf8b403da1d15ebba6a0b71c
innfi <innfi.world@gmail.com>
2021-04-04 21:42:48 +0900

    fix codes to build

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SignUp from './SignUp';
 import SignIn from './SignIn';
 import PublicPage from './PublicPage';
 import PrivatePage from './PrivatePage';
-import TimelineCard from './Timeline';
+import TimelineCards from './timeline/Timeline';
 
 
 export default function App() {
@@ -15,7 +15,7 @@ export default function App() {
               <Route path="/signin" exact={true} component={SignIn} />
               <Route path="/public" exact={true} component={PublicPage} />
               <Route path="/private" exact={true} component={PrivatePage} />
-              <Route path="/timeline" exact={true} component={TimelineCard} />
+              <Route path="/timeline" exact={true} component={TimelineCards} />
               <Redirect to="/signin" />
             </Switch>
         </>
--- src/PrivatePage.js
+++ src/PrivatePage.js
@@ -3,7 +3,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import { useSelector } from 'react-redux';
-import { TimelineCards } from './timeline/Timeline';
+import TimelineCards from './timeline/Timeline';
 
 
 export default function PrivatePage() {
