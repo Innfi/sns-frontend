1ba896b7ece04ebc0fbafa41bb6c379c08822965
innfi <innfi.world@gmail.com>
2021-05-22 20:11:52 +0900

    rewriting timelines

--- src/timeline/timeline.js
+++ src/timeline/timeline.js
@@ -1,17 +1,35 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { Card, CardActionArea, CardHeader, CardActions, CardContent, 
+    Button, Typography, Avatar, IconButton, Snackbar } from '@material-ui/core';
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import { loadTimelineThunk } from '../redux/reducks'
+import classes from '*.module.css';
 
 
 export default function TimelineCards() {
     const dispatch = useDispatch();
     const history = useHistory();
+    const authData = useSelector((state) => state.snsReducer.authData);
+    const emailFromRedux = authData.email;
 
     const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
+        const loadTimeline = async() => {
+            setIsLoading(true);
+            await dispatch(loadTimelineThunk({ email: emailFromRedux}, history));
+            setIsLoading(false);
+        };
 
+        loadTimeline();
     }, [dispatch]);
+
+    const userTimeline = useSelector((state) => state.snsReducer.timeline);
+
+    if(isLoading) return (
+        <div><p>loading...</p></div>
+    );
 
     return (
         <div>
@@ -19,3 +37,13 @@ export default function TimelineCards() {
         </div>
     );
 }
+
+const TimelineUnit = (timeline, key) => {
+    return (
+        <React.Fragment>
+            <Card className={classes.root}>
+
+            </Card>
+        </React.Fragment>
+    );
+};
